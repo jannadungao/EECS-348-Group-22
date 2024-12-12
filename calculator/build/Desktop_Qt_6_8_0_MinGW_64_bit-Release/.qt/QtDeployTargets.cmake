f7faa039300f3214e4a8b3a40582db7d84ba47a5
xNIGMAx <nigma.xii@gmail.com>
2024-12-11 19:55:58 -0600

    Final Version

---
+++ calculator/build/Desktop_Qt_6_8_0_MinGW_64_bit-Release/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_calculator_FILE C:/Users/Essaw/DT/Desktop/KU_Classes/Fall_2024/EECS_348/calculator/build/Desktop_Qt_6_8_0_MinGW_64_bit-Release/calculator.exe)
+set(__QT_DEPLOY_TARGET_calculator_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_calculator_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Qml.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Network.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
